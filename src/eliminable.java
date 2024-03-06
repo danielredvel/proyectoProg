baf01706746ef0d5dcd6118ae18fbd6128c89a62
Dani <daniel.redvel@educa.jcyl.es>
2024-03-06 12:16:47 +0100

    6/3/2024

--- src/eliminable.java
+++ src/eliminable.java
@@ -1,15 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public interface eliminable {
-    static void eliminar_departamento(int codigo){
-
+    static void eliminar(Object objeto_retirado, ArrayList lista){
+    lista.remove(objeto_retirado);
     }
-    static void eliminar_sala(int codigo){
-
-    }
-
-    static void cancelar_reserva(int sala,int tiempo){
-
-    }
-
 
 }
--- src/listar_salas.java
+++
@@ -1,7 +0,0 @@
-import java.util.ArrayList;
-
-public class listar_salas implements listable {
-    public static void listar(ArrayList lista) {
-
-    }
-}
