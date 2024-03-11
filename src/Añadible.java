567b9dd2ec2e21a8e535d4e5ea412dc0a8bfb80f
danielredvel <160476208+danielredvel@users.noreply.github.com>
2024-03-11 20:59:09 +0100

    Delete src directory

--- src/Añadible.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public interface Añadible {
-    static void añadir(Object objeto_añadido, ArrayList lista){
-        lista.add(objeto_añadido);
-    }
-
-}
--- src/Eliminable.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface Eliminable {
-    static void eliminar(Object objeto_retirado, ArrayList lista){
-    lista.remove(objeto_retirado);
-    }
-
-
-}
