94b1dd4e698a6a123eb015dc6046adfe813e8432
danielredvel <160476208+danielredvel@users.noreply.github.com>
2024-03-11 19:39:14 +0100

    Add files via upload

---
+++ proyectoProg/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.NoSuchElementException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu_inicio menuInicio =new Menu_inicio("menu"); //"menu" es la contraseña para el menú de Administrador
+
+        do {
+            Menu_inicio.salir=false;
+        try {
+            menuInicio.escribir_menu();
+        } catch (NumberFormatException nfe){
+            System.out.println("Indica un número");
+        } catch (NoSuchElementException nsee){
+            System.out.println("Error escaner");
+            }
+        catch (Exception e){
+            System.out.println("Error");
+        }
+        } while (!Menu_inicio.salir);
+    }
+}
